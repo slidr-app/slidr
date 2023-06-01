2a79d76b54d74ec96322a52afe39c394bf90b568
Cody Zuschlag <cody.zuschlag@gmail.com>
2023-06-01 19:30:47 +0200

    list presentation from db on homepage

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import {initializeApp} from 'firebase/app';
 import {getAuth} from 'firebase/auth';
+import {getStorage} from 'firebase/storage';
+import {getFirestore} from 'firebase/firestore/lite';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -18,3 +20,5 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const storage = getStorage(app);
+export const firestore = getFirestore(app);
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,69 @@
-import {useEffect, Fragment} from 'react';
+import {useEffect, Fragment, useState} from 'react';
 import {Link} from 'react-router-dom';
 import {Document, Page} from 'react-pdf';
 import * as pdfjs from 'pdfjs-dist';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import 'react-pdf/dist/esm/Page/TextLayer.css';
+import {list, ref} from 'firebase/storage';
+import {getDocs, collection} from 'firebase/firestore/lite';
 import {presentations, thumbs} from '../slides/presentation-urls.ts';
 import {
   pdfMessageProperties,
   pageMessageProperties,
 } from '../pdf/PdfMessages.tsx';
 import '../pdf/pdf.css';
+import {storage, firestore} from '../firebase.ts';
 
 const src = new URL('pdfjs-dist/build/pdf.worker.js', import.meta.url);
 pdfjs.GlobalWorkerOptions.workerSrc = src.toString();
 
 const presentationSlugs = Object.keys(presentations).sort();
 
+type Doc = {
+  id: string;
+};
+
+type PresentationData = {
+  title: string;
+  thumb: string;
+};
+
+type PresentationDoc = Doc & PresentationData;
+
 export default function Home() {
   useEffect(() => {
     document.title = `Present - Home`;
+  }, []);
+
+  const [presentations, setPresentations] = useState<PresentationDoc[]>([]);
+
+  useEffect(() => {
+    async function getPresentations() {
+      const querySnapshot = await getDocs(
+        collection(firestore, 'presentations'),
+      );
+
+      setPresentations(
+        querySnapshot.docs.map((doc) => {
+          const presentation: PresentationDoc = {
+            id: doc.id,
+            ...(doc.data() as PresentationData),
+          };
+          return presentation;
+        }),
+      );
+    }
+
+    async function getSlides() {
+      const result = await list(
+        ref(storage, 'presentations/jdXJqOTnjut0SrhjvSqp'),
+      );
+
+      console.log('result', result);
+    }
+
+    // Void getSlides();
+    void getPresentations();
   }, []);
 
   return (
@@ -80,6 +125,54 @@ export default function Home() {
                   </button>
                 </Link>
                 <Link to={`/${presentationSlug}/view`} className="">
+                  <button
+                    className="btn border-none shadow-none flex flex-col items-center p-2"
+                    type="button"
+                  >
+                    <div className="i-tabler-eyeglass" />
+                    <div className="text-base font-normal">audience</div>
+                  </button>
+                </Link>
+              </div>
+            </div>
+          </Fragment>
+        ))}
+        {presentations.map((presentation) => (
+          <Fragment key={presentation.id}>
+            <div className="w-full aspect-video shadow-primary border-primary md:rounded-r-none lt-md:rounded-b-none overflow-hidden bg-black flex items-center justify-center">
+              <div className="w-full h-full">
+                <Link to={`/${presentation.id}`} className="">
+                  <img src={presentation.thumb} />
+                </Link>
+              </div>
+            </div>
+
+            <div className="border-2 border-primary shadow-primary md:border-l-none md:rounded-l-none lt-md:border-t-none lt-md:rounded-t-none relative p-4 flex flex-col justify-center bg-black">
+              <div className="flex-grow flex items-center justify-center">
+                <Link to={`/${presentation.id}`}>
+                  <button
+                    className="btn border-none shadow-none p-2 flex flex-col items-center justify-center"
+                    type="button"
+                  >
+                    <div className="text-3xl">{presentation.title}</div>
+                    <div className="text-base font-normal">
+                      <div className="i-tabler-presentation mr-2" />
+                      present
+                    </div>
+                  </button>
+                </Link>
+              </div>
+              <div className="flex flex-row items-center justify-center flex-wrap justify-around">
+                <Link to={`/${presentation.id}/speaker`} className="flex">
+                  <button
+                    className="btn border-none shadow-none flex flex-col items-center p-2"
+                    type="button"
+                  >
+                    <div className="i-tabler-speakerphone" />
+                    <div className="text-base font-normal">speaker</div>
+                  </button>
+                </Link>
+                <Link to={`/${presentation.id}/view`} className="">
                   <button
                     className="btn border-none shadow-none flex flex-col items-center p-2"
                     type="button"
