66451654223275915362f6651e153403ae0201ba
Cody Zuschlag <cody.zuschlag@gmail.com>
2023-06-15 09:06:38 +0200

    improve caching of images

--- src/pages/Upload.tsx
+++ src/pages/Upload.tsx
@@ -165,7 +165,7 @@ export default function Export() {
       );
 
       await uploadBytes(pageStorageRef, pageBlob!, {
-        cacheControl: 'public, max-age=604800',
+        cacheControl: 'public, max-age=604800, immutable',
       });
       const pageUrl = await getDownloadURL(pageStorageRef);
       return pageUrl;
