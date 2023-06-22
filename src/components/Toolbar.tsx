854f2e036fb181df1d299b64704cfff8bca4a7c1
Cody Zuschlag <cody.zuschlag@gmail.com>
2023-06-22 16:51:18 +0200

    parallelogram toolbar

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -61,124 +61,129 @@ export default function Toolbar({
         forceToolbar && 'opacity-100',
       )}
     >
-      <div
-        className="flex flex-row bg-gray-800 rounded-md px-3 py-2 gap-3 shadow-teal-800 shadow-xl"
-        onClick={(event) => {
-          // Prevents navigating forward when toolbar is over the click to advance area
-          event.stopPropagation();
-        }}
-      >
-        <button
-          className="i-tabler-arrow-big-left-filled text-teal font-size-[4rem]"
-          type="button"
-          title="Previous"
-          onClick={onPrevious}
-        />
-        <button
-          className="i-tabler-arrow-big-right-filled text-teal font-size-[4rem]"
-          type="button"
-          title="Next"
-          onClick={onNext}
-        />
-        <button
-          className="i-tabler-arrow-bar-to-left text-teal font-size-[4rem]"
-          type="button"
-          title="Start"
-          onClick={onStart}
-        />
-        <button
-          className="i-tabler-arrow-bar-to-right text-teal font-size-[4rem]"
-          type="button"
-          title="End"
-          onClick={onEnd}
-        />
-        <div className="border-l-black border-l-1 mx-1" />
-        <a
-          className="flex"
-          href={`${window.location.origin}/s/${presentation?.id ?? ''}${
-            window.location.search
-          }`}
-          onClick={(event) => {
-            event.preventDefault();
-            window.open(
-              `${window.location.origin}/s/${presentation?.id ?? ''}${
+      <div className="flex flex row">
+        <div className="relative flex flex-row shadow-teal-800 shadow-xl">
+          <div className="w-0 h-0 border-1.25rem border-transparent border-b-gray-800 border-r-gray-800 content-empty" />
+          <div
+            className="flex flex-row bg-gray-800 px-3 py-2 gap-3"
+            onClick={(event) => {
+              // Prevents navigating forward when toolbar is over the click to advance area
+              event.stopPropagation();
+            }}
+          >
+            <button
+              className="i-tabler-arrow-big-left-filled text-teal font-size-[4rem]"
+              type="button"
+              title="Previous"
+              onClick={onPrevious}
+            />
+            <button
+              className="i-tabler-arrow-big-right-filled text-teal font-size-[4rem]"
+              type="button"
+              title="Next"
+              onClick={onNext}
+            />
+            <button
+              className="i-tabler-arrow-bar-to-left text-teal font-size-[4rem]"
+              type="button"
+              title="Start"
+              onClick={onStart}
+            />
+            <button
+              className="i-tabler-arrow-bar-to-right text-teal font-size-[4rem]"
+              type="button"
+              title="End"
+              onClick={onEnd}
+            />
+            <div className="border-l-black border-l-1 mx-1" />
+            <a
+              className="flex"
+              href={`${window.location.origin}/s/${presentation?.id ?? ''}${
                 window.location.search
-              }`,
-              undefined,
-              'popup',
-            );
-          }}
-        >
-          <button
-            className="i-tabler-speakerphone text-teal font-size-[4rem]"
-            type="button"
-            title="Open speaker view"
-          />
-        </a>
-        <a
-          className="flex"
-          href={`${window.location.origin}/i/${presentation?.id ?? ''}${
-            window.location.search
-          }`}
-          onClick={(event) => {
-            event.preventDefault();
-            window.open(
-              `${window.location.origin}/i/${presentation?.id ?? ''}${
+              }`}
+              onClick={(event) => {
+                event.preventDefault();
+                window.open(
+                  `${window.location.origin}/s/${presentation?.id ?? ''}${
+                    window.location.search
+                  }`,
+                  undefined,
+                  'popup',
+                );
+              }}
+            >
+              <button
+                className="i-tabler-speakerphone text-teal font-size-[4rem]"
+                type="button"
+                title="Open speaker view"
+              />
+            </a>
+            <a
+              className="flex"
+              href={`${window.location.origin}/i/${presentation?.id ?? ''}${
                 window.location.search
-              }`,
-            );
-          }}
-        >
-          <button
-            className="i-tabler-eyeglass text-teal font-size-[4rem]"
-            type="button"
-            title="Open audience view"
-          />
-        </a>
-        <Link
-          className="flex"
-          to={presentation ? `/e/${presentation.id}` : '/'}
-        >
-          <button
-            className="i-tabler-pencil text-teal font-size-[4rem]"
-            type="button"
-            title="Edit presentation"
-          />
-        </Link>
-        <Link className="flex" to="/">
-          <button
-            className="i-tabler-home text-teal font-size-[4rem]"
-            type="button"
-            title="Home"
-          />
-        </Link>
-        {/* <button
-          className="i-tabler-tools-off text-teal font-size-[4rem]"
-          type="button"
-          title="Hide toolbar"
-        /> */}
-        <div className="border-l-black border-l-1 mx-1" />
-
-        <button
-          className={clsx(
-            'font-size-[4rem]',
-            isFullscreen
-              ? 'i-tabler-arrows-diagonal-minimize-2'
-              : 'i-tabler-arrows-diagonal',
-            document.fullscreenEnabled ? 'text-teal' : 'text-gray-400',
-          )}
-          disabled={!document.fullscreenEnabled}
-          type="button"
-          title="Toggle fullscreen"
-          onClick={() => {
-            console.log('go fullscreen');
-            if (!document.fullscreenElement) {
-              void document.documentElement.requestFullscreen();
-            } else if (document.exitFullscreen) {
-              void document.exitFullscreen();
-            }
-          }}
-        />
+              }`}
+              onClick={(event) => {
+                event.preventDefault();
+                window.open(
+                  `${window.location.origin}/i/${presentation?.id ?? ''}${
+                    window.location.search
+                  }`,
+                );
+              }}
+            >
+              <button
+                className="i-tabler-eyeglass text-teal font-size-[4rem]"
+                type="button"
+                title="Open audience view"
+              />
+            </a>
+            <Link
+              className="flex"
+              to={presentation ? `/e/${presentation.id}` : '/'}
+            >
+              <button
+                className="i-tabler-pencil text-teal font-size-[4rem]"
+                type="button"
+                title="Edit presentation"
+              />
+            </Link>
+            <Link className="flex" to="/">
+              <button
+                className="i-tabler-home text-teal font-size-[4rem]"
+                type="button"
+                title="Home"
+              />
+            </Link>
+            {/* <button
+              className="i-tabler-tools-off text-teal font-size-[4rem]"
+              type="button"
+              title="Hide toolbar"
+            /> */}
+            <div className="border-l-black border-l-1 mx-1" />
+            <button
+              className={clsx(
+                'font-size-[4rem]',
+                isFullscreen
+                  ? 'i-tabler-arrows-diagonal-minimize-2'
+                  : 'i-tabler-arrows-diagonal',
+                document.fullscreenEnabled ? 'text-teal' : 'text-gray-400',
+              )}
+              disabled={!document.fullscreenEnabled}
+              type="button"
+              title="Toggle fullscreen"
+              onClick={() => {
+                console.log('go fullscreen');
+                if (!document.fullscreenElement) {
+                  void document.documentElement.requestFullscreen();
+                } else if (document.exitFullscreen) {
+                  void document.exitFullscreen();
+                }
+              }}
+            />
+          </div>
+        </div>
+        <div className="w-0 h-0 border-1.25rem border-transparent border-t-gray-800 border-l-gray-800 shadow-teal-800 content-none z-2" />
       </div>
     </div>
   );
