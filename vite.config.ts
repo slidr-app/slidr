b6189c55b48c216d0a22d4662f791e422a651d75
Cody Zuschlag <cody.zuschlag@gmail.com>
2023-07-03 14:47:58 +0200

    basic pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
+import {VitePWA as vitePWA} from 'vite-plugin-pwa';
 import unoCSS from 'unocss/vite';
 import {imagetools} from 'vite-imagetools';
 
 // https://vitejs.dev/config/
 export default defineConfig(({command, mode}) => {
   return {
-    plugins: [imagetools(), unoCSS(), react()],
+    plugins: [
+      imagetools(),
+      unoCSS(),
+      react(),
+      vitePWA({registerType: 'autoUpdate'}),
+    ],
   };
 });
