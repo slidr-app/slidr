7eb9397c7aa40b01e5f79ee055fbfe64806c1686
Cody Zuschlag <cody.zuschlag@gmail.com>
2023-07-13 11:05:01 +0200

    add ui + improve import export

--- src/components/slides/parse-notes.ts
+++ src/components/slides/parse-notes.ts
@@ -1,6 +1,6 @@
 import {type Note} from '../../../functions/src/presentation';
 
-export function importNotes(markdown: string): Note[] {
+export function importNotes(markdown: string, pageCount: number): Note[] {
   // We ensure a capture group around the entire expression so that it is still included in the String.split results
   const noteSections = markdown.split(/\n*(<!-- \+\d+(?:\+\d+)? -->)\n+/);
   const notes: Note[] = [];
@@ -8,7 +8,7 @@ export function importNotes(markdown: string): Note[] {
 
   for (
     let sectionIndex = 0;
-    sectionIndex < noteSections.length;
+    sectionIndex < noteSections.length && currentPageIndex < pageCount;
     sectionIndex++
   ) {
     const magicCommentRegEx =
@@ -19,7 +19,8 @@ export function importNotes(markdown: string): Note[] {
     if (match === null) {
       // Not matching at the start is the result of using String.split()
       // This indicates there was white space before the first magic comment in the markdown
-      if (sectionIndex !== 0) {
+      if (noteSection.length > 0) {
+        // If there is text, this means the notes were missing the first magic comment
         notes.push({pageIndices: [currentPageIndex], markdown: noteSection});
         currentPageIndex++;
       }
@@ -28,41 +29,58 @@ export function importNotes(markdown: string): Note[] {
     }
 
     const firstIndex =
-      currentPageIndex + Number.parseInt(match.groups!.offset, 10);
+      currentPageIndex + Number.parseInt(match.groups!.offset, 10) - 1;
 
     // Backfill any missing notes with empty notes, 1 note per index
     for (
-      let emptyIndex = currentPageIndex + 1;
-      emptyIndex < firstIndex;
-      emptyIndex++
+      ;
+      currentPageIndex < firstIndex && currentPageIndex < pageCount;
+      currentPageIndex++
     ) {
-      notes.push({pageIndices: [emptyIndex], markdown: ''});
-      currentPageIndex++;
+      notes.push({pageIndices: [currentPageIndex], markdown: ''});
+    }
+
+    if (currentPageIndex >= pageCount) {
+      break;
     }
 
     const pageLength = match.groups?.rangeOffset
       ? Number.parseInt(match.groups.rangeOffset, 10)
       : 1;
 
+    const lastIndex = firstIndex + pageLength - 1;
+
     // Generate the sequential indices between the firstIndex and the pageLength
-    const pageIndices = Array.from({length: pageLength}).map(
-      (_, index) => firstIndex + index,
-    ) as [number, ...number[]];
+    const pageIndices: number[] = [];
+    for (
+      ;
+      currentPageIndex <= lastIndex && currentPageIndex < pageCount;
+      currentPageIndex++
+    ) {
+      pageIndices.push(currentPageIndex);
+    }
 
     let markdown = '';
     // Grab the next section, but make sure it isn't a magic comment
     if (!magicCommentRegEx.test(noteSections[sectionIndex + 1])) {
       markdown = noteSections[sectionIndex + 1];
-      // Skip the next section
+      // Advance the section index for the next interation
       sectionIndex++;
     }
 
-    notes.push({pageIndices, markdown: markdown.trim()});
-
-    // Update the pageIndices index for the next note
-    currentPageIndex += pageLength - 1;
+    notes.push({
+      pageIndices: pageIndices as [number, ...number[]],
+      markdown: markdown.trim(),
+    });
   }
 
+  // Fill in any empty pages to reach the page count
+  // currentPageIndex++;
+  for (; currentPageIndex < pageCount; currentPageIndex++) {
+    notes.push({pageIndices: [currentPageIndex], markdown: ''});
+  }
+
+  // Slice off any extra pages that were parsed
   return notes;
 }
 
@@ -75,9 +93,9 @@ export function exportNotes(notes: Note[]): string {
         return undefined;
       }
 
-      const startIndex = currentOffset + note.pageIndices[0];
+      const startIndex = note.pageIndices[0] - currentOffset + 1;
       const noteLength = note.pageIndices.length;
-      currentOffset += noteLength - 1;
+      currentOffset += noteLength;
 
       return `<!-- +${startIndex}+${noteLength} -->\n${note.markdown}\n`;
     })
