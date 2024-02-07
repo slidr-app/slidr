d0c2385fa768204eaa93a1d6f77ff68416ad3e69
Cody Zuschlag <cody.zuschlag@gmail.com>
2024-02-07 10:19:01 +0000

    working poc with istanbul converter

--- src/pages/upload.e2e.ts
+++ src/pages/upload.e2e.ts
@@ -1,4 +1,6 @@
+import fs from 'node:fs';
 import {test as base, expect} from '@playwright/test';
+import v8toIstanbul from 'v8-to-istanbul';
 import {type LoginPage, loginPageFactory} from '../test/login-page';
 import {generateId} from '../test/id';
 
@@ -14,7 +16,12 @@ const test = base.extend<{loginPage: LoginPage}>({
   // },
 });
 
-test('upload button appears after signing in', async ({page, loginPage}) => {
+test.only('upload button appears after signing in', async ({
+  page,
+  loginPage,
+}, testInfo) => {
+  test.setTimeout(30_000);
+  await page.coverage.startJSCoverage();
   await page.goto('/');
   await expect(page.getByRole('button', {name: /upload/i})).not.toBeVisible();
 
@@ -23,10 +30,59 @@ test('upload button appears after signing in', async ({page, loginPage}) => {
   await loginPage.signInComplete();
   await page.goto('/');
   await expect(page.getByRole('button', {name: /upload/i})).toBeVisible();
+  const coverage = await page.coverage.stopJSCoverage();
+  // Absolute path to src folder "/Users/.../slidr/src"
+  const srcPath = new URL('..', import.meta.url);
+
+  let report: unknown = {};
+  for (const entry of coverage) {
+    // Absolute path to the coverage entry file "/Users/.../slidr/..."
+    const coveragePath = new URL(
+      `../..${new URL(entry.url).pathname}`,
+      import.meta.url,
+    );
+
+    // Determine if the file is in the src folder
+    const isInSrcFolder = coveragePath.pathname.startsWith(srcPath.pathname);
+
+    if (!isInSrcFolder) {
+      continue;
+    }
+
+    if (coveragePath.pathname.endsWith('.css')) {
+      continue;
+    }
+
+    // Console.log(coveragePath.pathname);
+    // console.log('entry', entry);
+    const converter = v8toIstanbul(coveragePath.pathname, 0, {
+      source: entry.source,
+    });
+    await converter.load();
+    converter.applyCoverage(entry.functions);
+    // Report[entry.source!] = converter.toIstanbul();
+
+    report = {...report, ...converter.toIstanbul()};
+    // Console.log(JSON.stringify(converter.toIstanbul()));
+  }
+
+  fs.mkdirSync('coverage/tmp', {recursive: true});
+  // Fs.writeFileSync(
+  //   'coverage/tmp/coverage.json',
+  //   JSON.stringify(report, null, 2),
+  // );
+  fs.writeFileSync(
+    `coverage/tmp/${testInfo.testId}.json`,
+    JSON.stringify(report, null, 2),
+  );
 });
 
-test('can upload and view presentation', async ({page, loginPage}) => {
+test.only('can upload and view presentation', async ({
+  page,
+  loginPage,
+}, testInfo) => {
   test.setTimeout(60_000);
+  await page.coverage.startJSCoverage();
 
   await loginPage.goto();
   await loginPage.signIn();
@@ -64,4 +120,45 @@ test('can upload and view presentation', async ({page, loginPage}) => {
   const page3 = page.getByAltText(/slide page 3/i);
   await expect(page3).toBeVisible();
   await expect(page3).toHaveScreenshot('page-3.png');
+  const coverage = await page.coverage.stopJSCoverage();
+  // Absolute path to src folder "/Users/.../slidr/src"
+  const srcPath = new URL('..', import.meta.url);
+
+  let report: unknown = {};
+  for (const entry of coverage) {
+    // Absolute path to the coverage entry file "/Users/.../slidr/..."
+    const coveragePath = new URL(
+      `../..${new URL(entry.url).pathname}`,
+      import.meta.url,
+    );
+
+    // Determine if the file is in the src folder
+    const isInSrcFolder = coveragePath.pathname.startsWith(srcPath.pathname);
+
+    if (!isInSrcFolder) {
+      continue;
+    }
+
+    if (coveragePath.pathname.endsWith('.css')) {
+      continue;
+    }
+
+    // Console.log(coveragePath.pathname);
+    // console.log('entry', entry);
+    const converter = v8toIstanbul(coveragePath.pathname, 0, {
+      source: entry.source,
+    });
+    await converter.load();
+    converter.applyCoverage(entry.functions);
+    // Report[entry.source!] = converter.toIstanbul();
+
+    report = {...report, ...converter.toIstanbul()};
+    // Console.log(JSON.stringify(converter.toIstanbul()));
+  }
+
+  fs.mkdirSync('coverage/tmp', {recursive: true});
+  fs.writeFileSync(
+    `coverage/tmp/${testInfo.testId}.json`,
+    JSON.stringify(report, null, 2),
+  );
 });
